a6746d5163411d0f97fd3b6e6bbc23f9b8ddf0a8
Widi-ps <69189062+Widi-ps@users.noreply.github.com>
2021-05-09 20:18:50 +0700

    kedua

--- intro-component-with-signup-form-master/script.js
+++ intro-component-with-signup-form-master/script.js
@@ -8,32 +8,31 @@ form.addEventListener("submit", (e) => {
   const email = form["email"].value;
   const password = form["password"].value;
 
-  alert(firstName+lastName+email+password);
+  alert(firstName + lastName + email + password);
 
-//   if (firstName === "") {
-//     addErrorTo("firstName", "First Name is required");
-//   }
-//   if (lastName === "") {
-//     addErrorTo("lastName", "Last Name is required");
-//   }
+  if (firstName === "") {
+    addErrorTo("firstName", "First Name is required");
+  }
+  if (lastName === "") {
+    addErrorTo("lastName", "Last Name is required");
+  }
 
-//   if (isValid(email)) {
-//     addErrorTo("email", "Email is required");
-//   }
+  if (isValid(email)) {
+    addErrorTo("email", "Email is required");
+  }
 
-//   if (password === "") {
-//     addErrorTo("password", "Password is required");
-//   }
-// });
+  if (password === "") {
+    addErrorTo("password", "Password is required");
+  }
 
-// function addErrorTo(field, message) {
-//   const small = form[field].parentNode.querySelector("small");
-//   small.innerText = message;
-//   small.style.opacity = "1";
-// }
+  function addErrorTo(field, message) {
+    const small = form[field].parentNode.querySelector("small");
+    small.innerText = message;
+    small.style.opacity = "1";
+  }
 
-// function isValid(email) {
-//   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-//   return re.test(String(email).toLowerCase());
-// 
+  function isValid(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
 });
