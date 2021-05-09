86f3383ea9ca2d0b43069c38975306eb60698913
Widi-ps <69189062+Widi-ps@users.noreply.github.com>
2021-05-09 19:11:32 +0700

    menambahkan js

---
+++ intro-component-with-signup-form-master/script.js
@@ -0,0 +1,4 @@
+const fisrt = document.getElementById("fisrtName");
+const last = document.getElementById("lasttName");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
