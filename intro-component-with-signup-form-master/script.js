0449ae494845f442c5d73c0e18921d86f625f9e1
Widi-ps <69189062+Widi-ps@users.noreply.github.com>
2021-05-09 19:58:07 +0700

    tambah js

--- intro-component-with-signup-form-master/script.js
+++ intro-component-with-signup-form-master/script.js
@@ -1,4 +1,39 @@
-const fisrt = document.getElementById("fisrtName");
-const last = document.getElementById("lasttName");
-const email = document.getElementById("email");
-const password = document.getElementById("password");
+const form = document.getElementById("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const firstName = form["fisrtName"].value;
+  const lastName = form["lasttName"].value;
+  const email = form["email"].value;
+  const password = form["password"].value;
+
+  alert(firstName+lastName+email+password);
+
+//   if (firstName === "") {
+//     addErrorTo("firstName", "First Name is required");
+//   }
+//   if (lastName === "") {
+//     addErrorTo("lastName", "Last Name is required");
+//   }
+
+//   if (isValid(email)) {
+//     addErrorTo("email", "Email is required");
+//   }
+
+//   if (password === "") {
+//     addErrorTo("password", "Password is required");
+//   }
+// });
+
+// function addErrorTo(field, message) {
+//   const small = form[field].parentNode.querySelector("small");
+//   small.innerText = message;
+//   small.style.opacity = "1";
+// }
+
+// function isValid(email) {
+//   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//   return re.test(String(email).toLowerCase());
+// 
+});
